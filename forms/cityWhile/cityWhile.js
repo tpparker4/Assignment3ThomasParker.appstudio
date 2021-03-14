bb5217a15c59edb377c555437ceb172306ab210f
tpparker4 <79471570+tpparker4@users.noreply.github.com>
2021-03-13 19:41:30 -0500

    cityWhile

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,23 @@
+/*
+let cities = []
+let doneWithCities = false
+let yesNo = ""
+
+while (doneWithCities == false) {
+  yesNo = prompt("Do you have more cities? Yes or No")
+  if (yesNo == "Yes") {
+    doneWithCities = false
+    newCity = prompt("Enter the next city")
+    cities.push(newCity)
+ } else {
+    doneWithCities = true
+  }
+}  
+
+let i = 0
+
+while (i < cities.length) {
+  console.log(cities[i].toLowerCase())
+  i++;
+  }
+*/
