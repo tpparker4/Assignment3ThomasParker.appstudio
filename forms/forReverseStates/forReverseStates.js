7fef2d14c2f265ba9e381275deda8323c42d0d29
tpparker4 <79471570+tpparker4@users.noreply.github.com>
2021-03-13 19:48:40 -0500

    functionCompare

--- forms/forReverseStates/forReverseStates.js
+++ forms/forReverseStates/forReverseStates.js
@@ -1,7 +1,8 @@
-
+/*
 let states = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas", "Oklahoma", "Texas"]
 states.reverse()
 
 for (i = 0; i < states.length; i++) {
   console.log(states[i])
   }
+*/
